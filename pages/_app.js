6ea37eaf416fe09af9fb8b0f3e95d03042237104
unknown <danielolahskybrow77@gmail.com>
2022-07-04 23:48:03 -0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { theme } from "../theme";
+import { store, persistor } from "../store";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return (
+    <>
+      <Head>
+        <title>Uniben-LMS</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {getLayout(<Component {...pageProps} />)}
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
